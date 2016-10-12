589a3c081af0d20485888f341abc5bcaea6d2639
Austen Lamacraft <al200@cam.ac.uk>
2016-10-12 12:04:35 +0100

    Update script

--- UpdateSite.sh
+++ UpdateSite.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #cron this script to update site
 git clone https://github.com/AustenLamacraft/Theories-of-Quantum-Matter.git
-rm -R /replicated/www/Theories-of-Quantum-Matter/docroot/docs/*
+rm -rf /replicated/www/Theories-of-Quantum-Matter/docroot/docs/*
 mv Theories-of-Quantum-Matter/docs/_site/* /replicated/www/Theories-of-Quantum-Matter/docroot/docs/
-rm -R Theories-of-Quantum-Matter
+rm -rf Theories-of-Quantum-Matter
